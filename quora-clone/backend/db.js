2e9ca0aee203deb74bb1e3ec7ed9e19a7dfcb8f8
kalash <kalashjain54@gmail.com>
2023-03-28 23:59:06 +0530

    adding backends

--- quora-clone/backend/db.js
+++ quora-clone/backend/db.js
@@ -1,6 +1,6 @@
 const mongoose =  require('mongoose')
 
-const url = "mongodb://KalashJain1010:Kalash@9754@ac-iqztluf-shard-00-00.46uutt7.mongodb.net:27017,ac-iqztluf-shard-00-01.46uutt7.mongodb.net:27017,ac-iqztluf-shard-00-02.46uutt7.mongodb.net:27017/?ssl=true&replicaSet=atlas-u1llao-shard-0&authSource=admin&retryWrites=true&w=majority"
+const url = "mongodb+srv://KalashJain1010:Kalash9754@cluster0.46uutt7.mongodb.net/${quora-clone}?retryWrites=true&w=majority"
 
 module.exports.connect =() =>{
     mongoose.connect(url, {
@@ -8,5 +8,5 @@ module.exports.connect =() =>{
         useUnifiedTopology: true
     }).then(()=>{
         console.log('mango connected');
-    }).catch((error) => console.log("error:", error))
+    }).catch((error) => console.log("error teri mommy:", error))
 }
